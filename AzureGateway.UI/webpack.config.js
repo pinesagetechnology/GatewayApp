03822aad696e46c9c9700d2c4b5365bb363ae3e5
Mohammad Ali RK <alir7812@gmail.com>
2025-10-08 23:19:12 +1300

    update webpack

--- AzureGateway.UI/webpack.config.js
+++ AzureGateway.UI/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = (env, argv) => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './public/index.html',
+        template: path.resolve(__dirname, 'public/index.html'),
         filename: 'index.html',
       }),
     ],
